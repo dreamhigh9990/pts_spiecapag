9d439f4490995b5c39a4a8a984f643c04907cf65
dreamhigh9990 <dreamhigh9990@gmail.com>
2023-08-31 18:16:15 +0900

    initial commit

---
+++ config/db - Copy.php
@@ -0,0 +1,29 @@
+<?php
+
+
+
+return [
+
+    'class' => 'yii\db\Connection',
+    // 'dsn' => 'mysql:host=localhost;dbname=pipeline',
+    // 'username' => 'root',
+    // 'password' => 'ZcR8422RtRCnHPJc',
+
+    'dsn' => 'mysql:host=localhost;dbname=pipe_test_v2',
+    'username' => 'root',
+    // 'password' => 'MqiyepeWDkiT9UbZ',
+    'password' => '',
+    'charset' => 'utf8',
+
+
+
+    // Schema cache options (for production environment)
+
+    //'enableSchemaCache' => true,
+
+    //'schemaCacheDuration' => 60,
+
+    //'schemaCache' => 'cache',
+
+];
+
